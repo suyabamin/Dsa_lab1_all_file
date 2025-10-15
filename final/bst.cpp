9b390af0c2e636cd8908de2c76928bfd1a026c76
suyab-amin <suyabamins@gmail.com>
2025-10-15 10:09:21 +0600

    dedfdifd

--- final/bst.cpp
+++ final/bst.cpp
@@ -53,6 +53,39 @@ node*max(node*root)
     }
     return root;
 }
+node*delete_node(node*root,int key){
+    if(root==NULL){
+        return root;
+    }if(key<root->value){
+        root->left=delete_node(root->left,key);
+    }else if(key>root->value){
+       root->right=delete_node(root->right,key);
+    }else {
+        if(root->left==NULL){
+           node*temp=root->left;
+           delete(root);
+           return temp;
+        }else if(root->right==NULL){
+            node*temp=root->right;
+            delete(root);
+            return temp;
+        }
+        node*temp;
+        temp=max(root->left);
+        root->value=temp->value;
+        root->left=delete_node(root->left,temp->value);
+    }
+    return root;
+}
+node*scarch(node*root,int key){
+if(root==NULL||root->value==key){
+    return root;
+}if(key<root->value){
+  return scarch(root->left,key);
+}if(key>root->value){
+  return  scarch(root->right,key);
+}
+}
 
 int main(){
     main_root=insertNode(main_root,6);
@@ -71,6 +104,9 @@ int main(){
       cout<<"postorder-";
     postorder(main_root);
     cout<<endl<<"max value is-"<<max(main_root)->value;
+     delete_node(main_root,5);
+         cout<<endl<<"preorder-";
+    preorder(main_root);
 
-   
+    cout<<endl<<scarch(main_root,7)->value<<"found";
 }
